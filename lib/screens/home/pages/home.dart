3e8299a3a0b6f3f8701118703d66245b48bc427d
zionmezba <mezbazion@gmail.com>
2025-03-09 11:22:08 +0600

    appbar widget

--- lib/screens/home/pages/home.dart
+++ lib/screens/home/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce_flutter/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -7,17 +8,43 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Mega Shop",
-          style: TextStyle(
-            fontFamily: "DMSans",
-            fontWeight: FontWeight.w700,
-            color: FColors.oceanBlue,
+      appBar: FAppBar(),
+    );
+  }
+}
+
+class FAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const FAppBar({
+    super.key,
+  });
+  
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        "Mega Shop",
+        style: TextStyle(
+          fontFamily: "DMSans",
+          fontWeight: FontWeight.w700,
+          color: FColors.oceanBlue,
+        ),
+      ),
+      centerTitle: true,
+      actions: [
+        Container(
+          padding: EdgeInsets.all(10),
+          child: Icon(Iconsax.notification_bing_outline),
+        ),
+        Container(
+          padding: EdgeInsets.all(10),
+          child: Icon(
+            HeroIcons.shopping_cart,
           ),
         ),
-        centerTitle: true,
-      ),
+      ],
     );
   }
 }
