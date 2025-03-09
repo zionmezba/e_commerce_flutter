dfb73e9dbc3317bfbf175dbf22b3bbb25369f05e
zionmezba <mezbazion@gmail.com>
2025-03-09 10:23:07 +0600

    folder structure

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FColors {
+  //Primary
+  static const primary = Color(0xFF5CC936);
+  static const primaryOrange = Color(0xFFFFC120);
+  static const primaryNavy = Color(0xFF0C1A30);
+  static const pureWhite = Color(0xFFFFFFFF);
+
+  // Secondary
+  static const secondary = Color(0xFF3A9B7A);
+  static const secondaryRed = Color(0xFFC93545);
+  static const darkGrey = Color(0xFF838589);
+  static const halfGrey = Color(0xFFC4C5C4);
+  static const softGrey = Color(0xFFEDEDED);
+  static const offGrey = Color(0xFFFAFAFA);
+
+  // Label Colors
+  static const offBlue = Color(0xFFEFF5FB);
+  static const offGreen = Color(0xFFEEFAF6);
+  static const offRed = Color(0xFFFCECEF);
+}
