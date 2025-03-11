0aa8434e518ce14ddc61867d45780a5ac134772d
Mezbaul Islam Zion <mezbazion@gmail.com>
2025-03-11 11:12:32 +0600

    Update home.dart

--- lib/screens/home/pages/home.dart
+++ lib/screens/home/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+
 import '../../../common/widgets/app_bar.dart';
 
 class Home extends StatelessWidget {
@@ -7,7 +9,11 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: FAppBar(),
+      appBar: FAppBar(
+        title: "Mega Shop",
+        firstIcon: Iconsax.notification_bing_outline,
+      ),
+      body: Text("ECOM"),
     );
   }
 }
